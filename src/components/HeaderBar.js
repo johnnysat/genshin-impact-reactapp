6f1727ef3f2ac3e5a5beed6303c9752a7f149790
Johnny <johnny_s.o@hotmail.com>
2022-12-04 11:24:16 -0300

    Header bom bootstrap adicionado

---
+++ src/components/HeaderBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './HeaderBar.css';
+
+function HeaderBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+
+        <Navbar.Brand href="#home">Genshin Impact - Aleatorizador</Navbar.Brand>
+          <Nav className="navbar_link">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+
+      </Container>
+    </Navbar>
+  );
+}
+
+export default HeaderBar;
