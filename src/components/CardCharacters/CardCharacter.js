b9dcbe0ce027f227a60055e578ec9310decc3167
Johnny <johnny_s.o@hotmail.com>
2022-12-09 21:57:04 -0300

    Adicionando pastas com images dos personagens

--- src/components/CardCharacters/CardCharacter.js
+++ src/components/CardCharacters/CardCharacter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import dailymission from '../../images/dailymission.PNG';
+import img from '../../images/dailymission.PNG';
 
 function CardCharacter({name, vision, weapon, nation}) {
 
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={dailymission}/>
+      <Card.Img variant="top" src={img}/>
       <Card.Body>   
         <Card.Title>GenshinDex</Card.Title>
         <Card.Text>
